940a8cc7868a26bc75ef2506049024399242fbcd
Vivek Rathod <rathod.vivek@gmail.com>
2025-07-24 20:07:31 -0400

    basic flake.nix

--- flake.nix
+++ flake.nix
@@ -1,146 +1,46 @@
-# ~/.config/nix/flake.nix
-
 {
-  description = "My system configuration";
+  description = "Example nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin = {
-        url = "github:LnL7/nix-darwin";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = {pkgs, ... }: {
-
-        services.nix-daemon.enable = true;
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility. please read the changelog
-        # before changing: `darwin-rebuild changelog`.
-        system.stateVersion = 4;
-
-        # The platform the configuration will be used on.
-        # If you're on an Intel system, replace with "x86_64-darwin"
-        nixpkgs.hostPlatform = "aarch64-darwin";
-
-        # allow Unfree packages like vscode 
-        nixpkgs.config = { allowUnfree = true; };
-
-        # Declare the user that will be running `nix-darwin`.
-        users.users.vrathod = {
-            name = "vrathod";
-            home = "/Users/vrathod";
-        };
-
-        # Create /etc/zshrc that loads the nix-darwin environment.
-        programs.zsh.enable = true;
-	
-        # install nix packages
-        environment.systemPackages = [ ];
-        
-        # install homebrew apps/packages
-        homebrew = {
-            enable = true;
-            onActivation.cleanup = "uninstall";
-
-            taps = [];
-            brews = [ "cowsay" ];
-            casks = [ "google-chrome" "whatsapp@beta" "sublime-merge" "joplin" "microsoft-remote-desktop" "zoom" "docker" "db-browser-for-sqlite"];
-        };
-
-        # enable touch id when using sudo
-        security.pam.enableSudoTouchIdAuth = true;
-    };
-    
-    # home-manager config
-    homeconfig = {pkgs, ...}: {
-        # this is internal compatibility configuration 
-        # for home-manager, don't change this!
-        home.stateVersion = "23.05";
-        # Let home-manager install and manage itself.
-        programs.home-manager.enable = true;
-
-        # \todo enable nerdfont? 
-        fonts.fontconfig.enable = true;
-
-        # install packages that are not specific to macOS here as home-manager is cross-platform
-        home.packages = with pkgs; [
-            pkgs.nerdfonts
-            pkgs.oh-my-zsh
-            pkgs.neofetch 
-            pkgs.vim 
-            pkgs.vscode
-            pkgs.git
-            pkgs.git-lfs
-            pkgs.warp-terminal
-            pkgs.nodejs_20
-	    pkgs.dotnet-sdk_8
-	   # pkgs.dotnet-sdk
-           # pkgs.docker
-           # pkgs.docker-compose
-           # pkgs.colima
-	    pkgs.grype
-	    pkgs.jq #command line json parser?
-	    pkgs.gitkraken
-	   # pkgs.sqlitebrowser
-	    pkgs.jetbrains.idea-community
+    configuration = { pkgs, ... }: {
+     # let determinate manage nix
+      nix.enable = false;	
+ 
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
         ];
 
-        home.sessionVariables = {
-            EDITOR = "vim";
-        };
-    
-        #configure dotfile for config as required
-        home.file.".vimrc".source = ./vim_configuration;
-	  
-   	    programs.zsh = {
-    		enable = true;
-            shellAliases = {
-                switch = "darwin-rebuild switch --flake ~/.config/nix";
-            };
-	    
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
 
-            oh-my-zsh = {
-                enable = true;
-                plugins = [ "git" "ssh-agent"];
-                theme = "agnoster";
-            };
-        };
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
 
-        programs.git = {
-            enable = true;
-	    lfs.enable = true;
-            userName = "Vivek Rathod";
-            userEmail = "vrathod@trustwave.com";
-            ignores = [ ".DS_Store" ];
-            extraConfig = {
-                init.defaultBranch = "master";
-                push.autoSetupRemote = true;
-            };
-        };
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
   {
-  darwinConfigurations."FLU-EN-9C973MY" = nix-darwin.lib.darwinSystem {
-        modules = [
-            configuration
-            home-manager.darwinModules.home-manager  {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.verbose = true;
-                home-manager.users.vrathod = homeconfig;
-            }
-        ];
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#FLU-EN-9C973MY
+    darwinConfigurations."FLU-EN-9C973MY" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
     };
   };
 }
