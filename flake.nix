c9c15473be41314e88c482d36a9d213bdd90b83d
Vivek Rathod <rathod.vivek@gmail.com>
2025-08-05 07:04:29 -0400

    enabling bunch of system nix packages like vscode and also enabling homebrew for some GUI apps like whatsapp etc

--- flake.nix
+++ flake.nix
@@ -18,14 +18,12 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [
+	  pkgs.vim
         ];
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -39,14 +37,99 @@
 
       # needed for home-manager (somehow uses this to decide the home dir and all)
       users.users.vrathod.home = /Users/vrathod;
+
+      # needed to allow unfree packages like vscode
+      nixpkgs.config.allowUnfree = true;
+
+      # enable zsh
+      programs.zsh.enable = true;
+	
+      # install homebrew apps/packages
+      homebrew = {
+         enable = true;
+         onActivation.cleanup = "uninstall";
+
+         taps = [];
+         brews = [ "cowsay" ];
+         casks = [ "google-chrome" "whatsapp@beta" "sublime-merge" "joplin" "microsoft-remote-desktop" "zoom" "docker" "db-browser-for-sqlite"];
+        };
+      
+      # required for homebrew enablement - apparently tells homebrew to run under this user
+      system.primaryUser = "vrathod";
+
+      # enable touch id when using sudo
+      security.pam.services.sudo_local.touchIdAuth = true;
+
     };
+
     # home-manager config
     homeconfig = {pkgs, ...}: {
         # this is internal compatibility configuration 
         # for home-manager, don't change this!
         home.stateVersion = "23.05";
-        # Let home-manager install and manage itself.
+        
+	# Let home-manager install and manage itself.
         programs.home-manager.enable = true;
+
+	# \todo enable nerdfont? 
+        fonts.fontconfig.enable = true;
+	
+        # install packages that are not specific to macOS here as home-manager is cross-platform
+        home.packages = [
+	    pkgs.nerd-fonts._0xproto
+            pkgs.nerd-fonts.droid-sans-mono
+            pkgs.oh-my-zsh
+            pkgs.neofetch 
+            pkgs.vim 
+            pkgs.vscode
+            pkgs.git
+            pkgs.git-lfs
+            pkgs.warp-terminal
+            pkgs.nodejs_20
+	    pkgs.dotnet-sdk_8
+	   # pkgs.dotnet-sdk
+           # pkgs.docker
+           # pkgs.docker-compose
+           # pkgs.colima
+	    pkgs.grype
+	    pkgs.jq #command line json parser?
+	    pkgs.gitkraken
+	   # pkgs.sqlitebrowser
+	    pkgs.jetbrains.idea-community
+        ];
+
+	home.sessionVariables = {
+            EDITOR = "vim";
+        };
+    
+        #configure dotfile for config as required
+        home.file.".vimrc".source = ./vim_configuration;
+	  
+   	programs.zsh = {
+    	   enable = true;
+           shellAliases = {
+              switch = "darwin-rebuild switch --flake ~/.config/nix";
+           };
+	    
+
+           oh-my-zsh = {
+             enable = true;
+             plugins = [ "git" "ssh-agent"];
+             theme = "agnoster";
+           };
+        };
+
+        programs.git = {
+            enable = true;
+            lfs.enable = true;
+            userName = "Vivek Rathod";
+            userEmail = "vrathod@trustwave.com";
+            ignores = [ ".DS_Store" ];
+            extraConfig = {
+                init.defaultBranch = "master";
+                push.autoSetupRemote = true;
+            };
+        };
    };
 in
   {
